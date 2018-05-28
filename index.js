ca27a0a530c7c6bc2f4d45d4aec11145316de00f
Denise Pendleton <dpendl1@hotmail.com>
2018-05-28 02:40:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Code your solution in this file!
 drivers = ["huey", "dewey", "louis"]
-const returnFirstTwoDrivers = function (newArray) 
+const returnFirstTwoDrivers = function (drivers) {
+  let newArray = []
+newArray.push(drivers[0])
+newArray.push(drivers[1])
+return newArray
+}
