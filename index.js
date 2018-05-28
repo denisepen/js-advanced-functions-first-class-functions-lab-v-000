b9b647f745e25dbddd4b0551af0e5a9560c46121
Denise Pendleton <dpendl1@hotmail.com>
2018-05-28 03:30:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,9 +28,6 @@ const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 function selectDifferentDrivers(drivers, choice){
-  //  if (choice == returnFirstTwoDrivers(drivers)){return choice}
-  //  else if
-  //  (choice == returnLastTwoDrivers(drivers)){return choice}
 return  choice == (returnFirstTwoDrivers(drivers)) || (returnLastTwoDrivers(drivers))
 // return choice
 
